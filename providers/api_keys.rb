954f3d4d7bea70b7b3f5b5a6a2816c181bc3dca1
imduffy15 <ian@ianduffy.ie>
2014-09-22 10:44:31 +0100

    Fix up some merge conflicts

--- providers/api_keys.rb
+++ providers/api_keys.rb
@@ -40,47 +40,47 @@ action :create do
     sleep(5)
     wait_count +=1
     if wait_count == 1
-      Chef::Log.info "Waiting CloudStack to start"
+      Chef::Log.info 'Waiting CloudStack to start'
     end
   end
 
   if cloudstack_api_is_running?
     # bypass the section if CloudStack is not running.
-    if ! @current_resource.admin_apikey.nil? or ! @current_resource.admin_secretkey.nil?
+    if !@current_resource.admin_apikey.nil? or !@current_resource.admin_secretkey.nil?
       # if keys attributes are empty search in Chef environment for other node having API-KEYS.
       other_nodes = search(:node, "chef_environment:#{node.chef_environment} AND cloudstack_admin_api_key:* NOT name:#{node.name}")
-      if ! other_nodes.empty?
-        @current_resource.admin_apikey(other_nodes.first["cloudstack"]["admin"]["api_key"])
-        @current_resource.admin_secretkey(other_nodes.first["cloudstack"]["admin"]["secret_key"])
+      unless other_nodes.empty?
+        @current_resource.admin_apikey(other_nodes.first['cloudstack']['admin']['api_key'])
+        @current_resource.admin_secretkey(other_nodes.first['cloudstack']['admin']['secret_key'])
       end
 
       if keys_valid?
         # API-KEYS from other nodes are valids, so updating current node attributes.
         #@current_resource.exists = true
-        Chef::Log.info "api keys: found valid keys from another node in the environment."
-        Chef::Log.info "api keys: updating node attributes"
-        node.normal["cloudstack"]["admin"]["api_key"] = @current_resource.admin_apikey
-        node.normal["cloudstack"]["admin"]["secret_key"] = @current_resource.admin_secretkey
+        Chef::Log.info 'api keys: found valid keys from another node in the environment.'
+        Chef::Log.info 'api keys: updating node attributes'
+        node.normal['cloudstack']['admin']['api_key'] = @current_resource.admin_apikey
+        node.normal['cloudstack']['admin']['secret_key'] = @current_resource.admin_secretkey
         node.save unless Chef::Config[:solo]
       end
     elsif keys_valid?
       # test API-KEYS on cloudstack, if they work, skip the section.
       @current_resource.exists = true
-      Chef::Log.info "api keys: are valid, nothing to do."
+      Chef::Log.info 'api keys: are valid, nothing to do.'
     else
-      if @current_resource.username == "admin"
+      if @current_resource.username == 'admin'
         admin_keys = retrieve_admin_keys(@current_resource.url, @current_resource.password)
         if admin_keys[:api_key].nil?
-          converge_by("Creating api keys for admin") do
+          converge_by('Creating api keys for admin') do
             admin_keys = generate_admin_keys(@current_resource.url, @current_resource.password)
-            Chef::Log.info "admin api keys: Generate new"
+            Chef::Log.info 'admin api keys: Generate new'
           end
         else
-          Chef::Log.info "admin api keys: use existing"
+          Chef::Log.info 'admin api keys: use existing'
         end
         #puts admin_keys
-        node.normal["cloudstack"]["admin"]["api_key"] = admin_keys[:api_key]
-        node.normal["cloudstack"]["admin"]["secret_key"] = admin_keys[:secret_key]
+        node.normal['cloudstack']['admin']['api_key'] = admin_keys[:api_key]
+        node.normal['cloudstack']['admin']['secret_key'] = admin_keys[:secret_key]
         node.save unless Chef::Config[:solo]
         $admin_apikey = admin_keys[:api_key]
         $admin_secretkey = admin_keys[:secret_key]
@@ -88,7 +88,7 @@ action :create do
       end
     end
   else
-    Chef::Log.error "CloudStack not running, cannot generate API keys."
+    Chef::Log.error 'CloudStack not running, cannot generate API keys.'
   end
 end
 
@@ -96,19 +96,19 @@ action :reset do
   # force generate new API keys
   #load_current_resource
   if cloudstack_is_running?
-    if @current_resource.username == "admin"
-      converge_by("Reseting admin api keys") do
+    if @current_resource.username == 'admin'
+      converge_by('Reseting admin api keys') do
         admin_keys = generate_admin_keys(@current_resource.url, @current_resource.password)
-        Chef::Log.info "admin api keys: Generate new"
-        node.normal["cloudstack"]["admin"]["api_key"] = admin_keys[:api_key]
-        node.normal["cloudstack"]["admin"]["secret_key"] = admin_keys[:secret_key]
+        Chef::Log.info 'admin api keys: Generate new'
+        node.normal['cloudstack']['admin']['api_key'] = admin_keys[:api_key]
+        node.normal['cloudstack']['admin']['secret_key'] = admin_keys[:secret_key]
         node.save unless Chef::Config[:solo]
         $admin_apikey = admin_keys[:api_key]
         $admin_secretkey = admin_keys[:secret_key]
       end
     end
   else
-    Chef::Log.error "CloudStack not running, cannot generate API keys."
+    Chef::Log.error 'CloudStack not running, cannot generate API keys.'
   end
 
   puts $admin_apikey
@@ -116,7 +116,7 @@ end
 
 
 def generate_admin_keys(url='http://localhost:8080/client/api', password='password')
-  login_params = { :command => "login", :username => "admin", :password => password, :response => "json" }
+  login_params = {:command => 'login', :username => 'admin', :password => password, :response => 'json'}
   # create sessionkey and cookie of the api session initiated with username and password
   uri = URI(url)
   uri.query = URI.encode_www_form(login_params)
@@ -124,21 +124,21 @@ def generate_admin_keys(url='http://localhost:8080/client/api', password='passwo
   res = http.get(uri.request_uri)
   get_keys_params = {
       :sessionkey => JSON.parse(res.body)['loginresponse']['sessionkey'],
-      :command => "registerUserKeys",
-      :response => "json",
-      :id => "2"
+      :command => 'registerUserKeys',
+      :response => 'json',
+      :id => '2'
   }
 
   # use sessionkey + cookie to generate admin API and SECRET keys.
   uri2 = URI(url)
   uri2.query = URI.encode_www_form(get_keys_params)
   sleep(2) # add some delay to have the session working 
-  query_for_keys = http.get(uri2.request_uri, { 'Cookie' => res.response['set-cookie'].split('; ')[0] })
+  query_for_keys = http.get(uri2.request_uri, {'Cookie' => res.response['set-cookie'].split('; ')[0]})
 
-  if users.code == "200"
+  if users.code == '200'
     keys = {
-        :api_key => JSON.parse(query_for_keys.body)["registeruserkeysresponse"]["userkeys"]["apikey"],
-        :secret_key => JSON.parse(query_for_keys.body)["registeruserkeysresponse"]["userkeys"]["secretkey"]
+        :api_key => JSON.parse(query_for_keys.body)['registeruserkeysresponse']['userkeys']['apikey'],
+        :secret_key => JSON.parse(query_for_keys.body)['registeruserkeysresponse']['userkeys']['secretkey']
     }
   else
     Chef::Log.info "Error creating keys errorcode: #{users.code}"
@@ -150,7 +150,9 @@ end
 def keys_valid?
   # Test if current defined keys from Chef are valid
   #
-  unless @current_resource.admin_apikey.nil? or @current_resource.admin_secretkey.nil?
+  if @current_resource.admin_apikey.nil? or @current_resource.admin_secretkey.nil?
+    return false
+  else
     # return false if one key is empty
     require 'cloudstack_ruby_client'
     begin
@@ -164,14 +166,12 @@ def keys_valid?
     else
       return true
     end
-  else
-    return false
   end
 end
 
 
 def retrieve_admin_keys(url='http://localhost:8080/client/api', password='password')
-  login_params = { :command => "login", :username => "admin", :password => password, :response => "json" }
+  login_params = {:command => 'login', :username => 'admin', :password => password, :response => 'json'}
   # create sessionkey and cookie of the api session initiated with username and password
   uri = URI(url)
   uri.query = URI.encode_www_form(login_params)
@@ -179,19 +179,19 @@ def retrieve_admin_keys(url='http://localhost:8080/client/api', password='passwo
   res = http.get(uri.request_uri)
   get_keys_params = {
       :sessionkey => JSON.parse(res.body)['loginresponse']['sessionkey'],
-      :command => "listUsers",
-      :response => "json",
-      :id => "2"
+      :command => 'listUsers',
+      :response => 'json',
+      :id => '2'
   }
   # use sessionkey + cookie to generate admin API and SECRET keys.
   uri2 = URI(url)
   uri2.query = URI.encode_www_form(get_keys_params)
   sleep(2) # add some delay to have the session working 
-  users = http.get(uri2.request_uri, { 'Cookie' => res.response['set-cookie'].split('; ')[0] })
-  if users.code == "200"
+  users = http.get(uri2.request_uri, {'Cookie' => res.response['set-cookie'].split('; ')[0]})
+  if users.code == '200'
     keys = {
-        :api_key => JSON.parse(users.body)["listusersresponse"]["user"].first["apikey"],
-        :secret_key => JSON.parse(users.body)["listusersresponse"]["user"].first["secretkey"]
+        :api_key => JSON.parse(users.body)['listusersresponse']['user'].first['apikey'],
+        :secret_key => JSON.parse(users.body)['listusersresponse']['user'].first['secretkey']
     }
   else
     Chef::Log.info "Error creating keys errorcode: #{users.code}"
